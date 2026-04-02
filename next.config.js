d4a21873a228812720ab8b44cb0d519141fde225
CivicIntelCanada <musemidnight6969@gmail.com>
2026-04-02 13:46:11 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
+  },
+  trailingSlash: true,
+  async redirects() {
+    return []
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
